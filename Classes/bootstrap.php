43053ec485516b51ae2d2e7a24f2baae48b82125
Jan Runte <nimayneb@users.noreply.github.com>
2016-03-08 16:13:10 +0100

    fixed wrong autoload relative path

--- Classes/bootstrap.php
+++ Classes/bootstrap.php
@@ -10,7 +10,7 @@ function includeFileIfExists($file) {
     return (true === file_exists($file)) ? include_once $file : false;
 }
 
-if (false === ($autoload = includeFileIfExists(sprintf('%s/../vendor/autoload.php', __DIR__)))) {
+if (false === ($autoload = includeFileIfExists(sprintf('%s/../../../vendor/autoload.php', __DIR__)))) {
     echo "Please install composer dependencies first!\n\n";
     exit(1);
 }
